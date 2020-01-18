e03c4147703d5f0496cea04e53dc6ae2cfa4dd53
Klemen Tušar <techouse@gmail.com>
2020-01-18 10:58:30 +0000

    :bookmark: Bump to v1.0.1

--- termania/__version__.py
+++ termania/__version__.py
@@ -2,7 +2,7 @@
 __title__ = "termania"
 __description__ = "A Python interface for the Termania.net dictionary API"
 __url__ = "https://github.com/techouse/termania-api"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Klemen Tušar"
 __author_email__ = "techouse@gmail.com"
 __license__ = "MIT"
